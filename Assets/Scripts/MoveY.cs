11d847614d0aebe837b37b9362fcf7a44ce712c5
dienanh2210 <anhdien2210@gmail.com>
2018-10-17 16:04:48 +0700

    riseup

---
+++ Assets/Scripts/MoveY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveY : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(speed*Time.deltaTime,0,0);
+	}
+}
